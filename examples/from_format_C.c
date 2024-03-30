37ce766b3697c52590c65ba8bea4b89e72f608ee
Tobin Cavanaugh <tobincavanaugh@gmail.com>
2024-03-29 22:44:06 -0500

    Hmmm im going down a rabbithole, this might not be the most pragmatic

--- examples/from_format_C.c
+++ examples/from_format_C.c
@@ -5,7 +5,7 @@
 #include "../fstr.h"
 
 int main(){
-    fstr * str = fstr_from_C("abc123");
+    fstr * str = fstr_from_format_C("%02d-%02d-%d", 4, 12, 2005);
     fstr_println(str);
     fstr_free(str);
 }
--- examples/from_length.c
+++ examples/from_length.c
@@ -5,7 +5,7 @@
 #include "../fstr.h"
 
 int main(){
-    fstr * str = fstr_from_C("abc123");
+    fstr * str = fstr_from_length(5, '0');
     fstr_println(str);
     fstr_free(str);
 }
