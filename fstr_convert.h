6867c502201d49f45a9589a50a3afba13ad79e3b
Tobin Cavanaugh <tobincavanaugh@gmail.com>
2024-05-28 10:13:07 -0700

    didnt get pushed

---
+++ fstr_convert.h
@@ -0,0 +1,18 @@
+//
+// Created by tobin on 3/18/2024.
+//
+
+#ifndef FSTR_FSTR_H
+
+#include "fstr.h"
+
+#endif //FSTR_FSTR_H
+
+#ifndef FSTR_FSTR_PARSE_H
+#define FSTR_FSTR_PARSE_H
+
+fstr_result fstr_to_i64(const fstr* str);
+fstr_result fstr_to_double(fstr* str);
+fstr_result fstr_u64_from_bin(fstr* str);
+
+#endif //FSTR_FSTR_PARSE_H
